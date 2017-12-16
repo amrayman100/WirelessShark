a92962a183c40faf4a0510d254d697888ef7e129
MohamadTarek <mohamad.elzeidy@gmail.com>
2017-12-16 23:49:06 +0200

    updated main menu interface

--- WirelessShark/src/wirelessshark/MainMenuController.java
+++ WirelessShark/src/wirelessshark/MainMenuController.java
@@ -6,6 +6,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +15,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
@@ -28,95 +31,43 @@ public class MainMenuController implements Initializable {
     @FXML
     ImageView cover;
     @FXML 
-    JFXButton dev0 = new JFXButton();
-    @FXML 
-    JFXButton dev1 = new JFXButton();
-    @FXML 
-    JFXButton dev2 = new JFXButton();
-    @FXML 
-    JFXButton dev3 = new JFXButton();
-    @FXML 
-    JFXButton dev4 = new JFXButton();
+    ComboBox list = new ComboBox();
+    
     @FXML
-    JFXTextField title = new JFXTextField();
+    JFXButton start = new JFXButton();
     
     
-    //image = new Image("/Wireless/"+image);
+ 
     static List<PcapIf> alldevs = new ArrayList<PcapIf>();
          boolean cancelled = false;
          static StringBuilder errbuf = new StringBuilder();
-         static int r = Pcap.findAllDevs(alldevs, errbuf);   
-    
+         static int r = Pcap.findAllDevs(alldevs, errbuf);  
+         ObservableList<String> options = FXCollections.observableArrayList();
+        
     
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+         for (PcapIf device : alldevs) {  
+            options.add(device.getDescription());
+        }  
          // cover.setImage("Shark.png");
-        title.setEditable(false);
-        if(alldevs.get(0).getDescription().equalsIgnoreCase("Microsoft")){
-             dev0.setText(alldevs.get(0).getDescription()+" #0");
-        }
-        else{
-              dev0.setText(alldevs.get(0).getDescription());
-        }
+     
+        list.setItems(options);
+    
         
-         if(alldevs.get(1).getDescription().equalsIgnoreCase("Microsoft")){
-             dev1.setText(alldevs.get(1).getDescription()+" #1");
-        }
-        else{
-              dev1.setText(alldevs.get(1).getDescription());
-        }
-         
-          if(alldevs.get(2).getDescription().equalsIgnoreCase("Microsoft")){
-             dev2.setText(alldevs.get(2).getDescription()+" #2");
-        }
-        else{
-              dev2.setText(alldevs.get(2).getDescription());
-        }
-          
-           if(alldevs.get(3).getDescription().equalsIgnoreCase("Microsoft")){
-             dev3.setText(alldevs.get(3).getDescription()+" #3");
-        }
-        else{
-              dev3.setText(alldevs.get(3).getDescription());
-        }
-           
-           
-           if(alldevs.get(4).getDescription().equalsIgnoreCase("Microsoft")){
-             dev4.setText(alldevs.get(4).getDescription()+" #4");
-        }
-        else{
-              dev4.setText(alldevs.get(4).getDescription());
-        }
-          
      
        
     }    
     
-     @FXML
-    private void sniff0(ActionEvent event){
-        WirelessShark.device = alldevs.get(0);
-         FXMLLoader loader = new FXMLLoader();
-           loader.setLocation(getClass().getResource("Shark.fxml"));
-         try {
-             loader.load();       
-        } catch(Exception e) {
-           e.printStackTrace();
-          }
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Parent root1 = loader.getRoot();            
-        Scene scene1 = new Scene(root1);
-        stage.setScene(scene1);
-        stage.show();
-        stage.setResizable(false);
-
-      }
+   
     
      @FXML
     
-     private void sniff1(ActionEvent event){
-        WirelessShark.device = alldevs.get(1);
+     private void start(ActionEvent event){
+        
+         int a = list.getSelectionModel().getSelectedIndex();
+        WirelessShark.device = alldevs.get(a);
               FXMLLoader loader = new FXMLLoader();
            loader.setLocation(getClass().getResource("Shark.fxml"));
          try {
@@ -133,63 +84,8 @@ public class MainMenuController implements Initializable {
         
       }
      
-      @FXML
-      private void sniff2(ActionEvent event){
-           WirelessShark.device = alldevs.get(2);
-               FXMLLoader loader = new FXMLLoader();
-           loader.setLocation(getClass().getResource("Shark.fxml"));
-         try {
-             loader.load();       
-        } catch(Exception e) {
-           e.printStackTrace();
-          }
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Parent root1 = loader.getRoot();            
-        Scene scene1 = new Scene(root1);
-        stage.setScene(scene1);
-        stage.show();
-        stage.setResizable(false);
      
-      }
-       @FXML
-       private void sniff3(ActionEvent event){
-            WirelessShark.device = alldevs.get(3);
-                FXMLLoader loader = new FXMLLoader();
-           loader.setLocation(getClass().getResource("Shark.fxml"));
-         try {
-             loader.load();       
-        } catch(Exception e) {
-           e.printStackTrace();
-          }
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Parent root1 = loader.getRoot();            
-        Scene scene1 = new Scene(root1);
-        stage.setScene(scene1);
-        stage.show();
-        stage.setResizable(false);
-     
-      }
-        @FXML
-        private void sniff4(ActionEvent event){
-             WirelessShark.device = alldevs.get(4);
-                 FXMLLoader loader = new FXMLLoader();
-           loader.setLocation(getClass().getResource("Shark.fxml"));
-         try {
-             loader.load();       
-        } catch(Exception e) {
-           e.printStackTrace();
-          }
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Parent root1 = loader.getRoot();            
-        Scene scene1 = new Scene(root1);
-        stage.setScene(scene1);
-        stage.show();
-        stage.setResizable(false);
-     
-       }
     
     
     
 }
-
-
