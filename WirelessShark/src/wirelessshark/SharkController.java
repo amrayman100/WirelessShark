1da828aebe15f76254c45a0b5d9401311d56a6e6
amraymankamal <amrayman1996@yahoo.com>
2017-12-20 19:38:28 +0000

    ,

--- WirelessShark/src/wirelessshark/SharkController.java
+++ WirelessShark/src/wirelessshark/SharkController.java
@@ -1,27 +1,61 @@
+
 package wirelessshark;
 
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSnackbar;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import java.awt.Insets;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.jnetpcap.packet.PcapPacket;
 
 
 public class SharkController implements Initializable {
-
-  
+ @FXML
+public JFXButton Start = new JFXButton();
+  @FXML
+public JFXButton Stop = new JFXButton();
+   @FXML
+public JFXButton open = new JFXButton();
 public TextArea text;
-  public Sniffing  s = new Sniffing();   
+ public Sniffing  s = new Sniffing();
+ @FXML
+ private AnchorPane pane = new AnchorPane();
+ 
+ // public Dumping d = new Dumping();  
      @FXML
     private TableView<packetInfo> result;
   @FXML
@@ -31,7 +65,8 @@ public TextArea text;
   
    @FXML
     private TableColumn<packetInfo, String> No,Time,Dest,Source,Length,Prot,Info;
-  
+       final FileChooser fileChooser = new FileChooser();
+       readFile r;
    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -70,19 +105,34 @@ public TextArea text;
         
        result.setItems(filteredData);
         
-        
+        //s.start();
+     
+        Start.setDisable(true);
+        open.setDisable(true);
     
     } 
      @FXML
         private void Start(ActionEvent event){
-
+             JFXSnackbar snack = new JFXSnackbar(pane);
+             snack.show("Packet Capturing Started",3000);
+        WirelessShark.content.clear();
         s.start();
+       // d.start();
+        Start.setDisable(true);
+        open.setDisable(true);
+        s.count = 0;
+       // d.count++;
         
+       
+         
     }
         
        @FXML
         private void selected(){
            packetInfo selected = result.getSelectionModel().getSelectedItem();
+           if(selected == null){
+               return;
+           }
            info.setText(selected.packet.toString());
       
           
@@ -94,27 +144,61 @@ public TextArea text;
        private void Stop(ActionEvent event){
           
      
-            s.cancel();
+             s.cancel();
              s.reset();
+            // d.cancel();
+            // d.reset();
             System.out.println("Paused");
-           
-          
+           Start.setDisable(false);
+           open.setDisable(false);
+            JFXSnackbar snack = new JFXSnackbar(pane);
+            snack.show("Packet Capturing Stopped",3000);
        
       }  
-       @FXML
-          public void fiterDisplay(){
      
-}
-          
-  
-        
+       
+           @FXML
+       private void open(ActionEvent event){
+           
+           
+            
+     
+             WirelessShark.content.clear();
+           
+          Stage primaryStage = new Stage();
+      
+                  File file = fileChooser.showOpenDialog(primaryStage);
+                  //FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter( "CAP","*.cap");
+                  //FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter( "CAP","*.cap");
+                  //fileChooser.getExtensionFilters().add(extFilter);
+                if(file == null){
+                   // labelSelectedDirectory.setText("No Directory selected");
+                }else{
+                    r = new readFile(file.getAbsoluteFile().getAbsolutePath());
+                    System.out.println(file.getAbsoluteFile());
+               
+                       r.start();
+                 
+                }
+                
+                 JFXSnackbar snack = new JFXSnackbar(pane);
+             snack.show("Packets Loading",3000);
+            Start.setDisable(false);
+    
+       
+       }
+          @FXML
+       private void back(ActionEvent event) throws Exception{
+           
+            WirelessShark.content.clear();
+              Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Scene scene = new Scene(root);
+         Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+   
+       stage.setScene(scene);
+       
+       stage.show();
+       }
+       
        
 }
-
-
-
-
-
-
-
-
